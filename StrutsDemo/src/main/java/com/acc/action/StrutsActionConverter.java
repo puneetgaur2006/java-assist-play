f6c91a6923b42d074bfb20bbdfde4ff655fbcfdf
puneetgaur2006 <puneetgaur2006@gmail.com>
2020-07-09 17:24:57 +0530

    Fixed package import dynamically

--- StrutsDemo/src/main/java/com/acc/action/StrutsActionConverter.java
+++ StrutsDemo/src/main/java/com/acc/action/StrutsActionConverter.java
@@ -1,9 +1,13 @@
 package com.acc.action;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.util.StringUtils;
@@ -21,31 +25,46 @@ public class StrutsActionConverter {
 
 	private static final String controllerAnnotation = "org.springframework.stereotype.Controller";
 	private static final String methodName = "execute";
+	private static final String modelPackage = "org.springframework.ui";
 
 	public static void main(String[] args) throws Exception, Exception {
 		try {
 
 			// Read a class
-			// TODO: If this works, read the entire folder structure and apply rules
-			//
 
 			int numberOfActionFiles = new File("src/main/java/com/acc/action/").listFiles().length;
 			File[] files = new File("src/main/java/com/acc/action/").listFiles();
+
+			// Reading class as a file
+			List<String> imports = new ArrayList<String>();
+			for (int iCount = 0; iCount < numberOfActionFiles; iCount++) {
+				@SuppressWarnings("resource")
+				BufferedReader br = new BufferedReader(new FileReader(files[iCount]));
+				String st;
+
+				while ((st = br.readLine()) != null) {
+					if (st.startsWith("import")) {
+						// Capture the string
+						imports.add(st);
+					}
+				}
+			}
+
+			// Reading and storing class as string
 			for (int i = 0; i < numberOfActionFiles; i++) {
 				String legacyCode = readClass(files[i].getName());
 
 				// Check if source class extends Action
-				boolean needsRefactoring = legacyCode.contains("extends Action") && !"StrutsActionConverter.java".equals(files[i].getName());
+				boolean needsRefactoring = legacyCode.contains("extends Action")
+						&& !"StrutsActionConverter.java".equals(files[i].getName());
 
 				if (needsRefactoring) {
 					// Copy the contents between {}
 					int methodIndex = legacyCode.indexOf("{", legacyCode.indexOf("{") + 1);
 					String methodLines = legacyCode.substring(methodIndex + 1);
 					methodLines = methodLines.substring(0, methodLines.length() - 2);
-					// System.out.println("Method lines " + methodLines.substring(0,
-					// methodLines.length()-2));
 					System.out.println("Attempting to generate controller for " + files[i].getName());
-					Class clazz = generateClass(legacyCode, files[i].getName(), methodName, methodLines);
+					Class<?> clazz = generateClass(legacyCode, files[i].getName(), methodName, methodLines, imports);
 					System.out.println("Class generated but will require manual fixes: " + clazz.getName());
 				} else {
 					System.out.println("No need to refactor... " + files[i].getName());
@@ -73,14 +92,30 @@ public class StrutsActionConverter {
 		return sb.toString();
 	}
 
-	public static Class generateClass(String legacyCode, String className, String methodName, String methodLines)
-			throws CannotCompileException, Exception, IOException {
+	public static Class<?> generateClass(String legacyCode, String className, String methodName, String methodLines,
+			List<String> imports) throws CannotCompileException, Exception, IOException {
 		ClassPool pool = ClassPool.getDefault();
-		pool.importPackage("com.acc.form");
-		pool.importPackage("com.acc.service");
-		pool.importPackage("org.apache.struts.action");
-		pool.importPackage("org.springframework.ui");
-		pool.importPackage("javax.servlet.http");
+		String s = null;
+		// Read package import statements and configure the pool
+		for (String packageName : imports) {
+			// System.out.println(packageName);
+			String processedPackage = packageName.substring(7, packageName.indexOf(";"));
+			char[] ch = processedPackage.toCharArray();
+			char cj[] = new char[25000];
+			for (int i = 0; i < ch.length; i++) {
+				if (Character.isUpperCase(ch[i])) {
+					break;
+				}
+				cj[i] = ch[i];
+				s = new String(cj);
+				// System.out.println(s);
+			}
+			s = s.trim();
+			String finalPackage = s.substring(0, s.length() - 1);
+			// System.out.println(finalPackage);
+			pool.importPackage(finalPackage);
+			pool.importPackage(modelPackage);// This is required to access Model object
+		}
 		int len = className.length();
 		className = className.substring(0, len - 5);
 		CtClass cc = pool.makeClass(className);
@@ -95,14 +130,14 @@ public class StrutsActionConverter {
 		cc.getClassFile().addAttribute(attr);
 
 		// TODO: playaround
-		method.append("public String ").append(methodName).append(
-				"(ActionForm form, Model model, ActionMapping mapping, HttpServletRequest request) {");
+		method.append("public String ").append(methodName)
+				.append("(ActionForm form, Model model, ActionMapping mapping, HttpServletRequest request) {");
 		String replaceReturnStatement = StringUtils.replace(methodLines, "mapping.findForward(\"success\")",
 				"\"view\"");
 		methodLines = replaceReturnStatement;
 		method.append(methodLines);
 		method.append("}");
-		//System.out.println(method.toString());
+		// System.out.println(method.toString());
 		cc.addMethod(CtMethod.make(method.toString(), cc));
 		cc.writeFile();
 		return cc.toClass();
